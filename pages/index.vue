6bdcc040f37d1a018bfa8d657b8d4b5dcaa6a585
Brian Lawrence <Bobukanator@gmail.com>
2022-04-21 16:32:14 +0200

    Added load from questions json

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,22 @@
 <template>
   <section class="section">
-    <thequestion question="this is a question"></thequestion>
+    <thequestion :question="questions[0].question"></thequestion>
   </section>
 </template>
 
 <script>
 import thequestion from "~/components/thequestion";
+import questionData from "~/static/questions.json";
 
 export default {
   name: "MainPage",
   components: {
     thequestion,
+  },
+  data() {
+    return {
+      questions: questionData.questions,
+    };
   },
 };
 </script>
