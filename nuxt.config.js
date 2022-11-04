b88117dfd37a2b9fd4a7669c5b06d2c500bcc93f
Brian Lawrence <Bobukanator@gmail.com>
2022-11-04 16:43:12 +0100

    cleaned up nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,5 +1,5 @@
 export default {
-  // Global page headers: https://go.nuxtjs.dev/config-head
+  components: true,
   head: {
     title: 'Powerful Questions',
     htmlAttrs: {
@@ -15,32 +15,19 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
-
   env: {
     BASE_URL: process.env.BASE_URL || 'http://localhost:3000'
   },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
   css: ['@/assets/css/main.scss'
   ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: ['~/plugins/dataApi', '~/plugins/gs.client'],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
   ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/buefy
-    'nuxt-buefy', "~/modules/questions"
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
+  modules: ['nuxt-buefy', "~/modules/questions"],
   build: {
+    extractCSS: true,
+    loaders: {
+      limit: 0,
+    }
   }
 }
